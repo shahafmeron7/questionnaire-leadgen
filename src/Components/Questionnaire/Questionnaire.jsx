e5778fbf56792263593f0f9dcbccf774ab2323d8
Shahaf Meron <shahaf.meron@RBM041.local>
2024-04-08 18:25:13 +0300

    changes

--- src/Components/Questionnaire/Questionnaire.jsx
+++ src/Components/Questionnaire/Questionnaire.jsx
@@ -5,7 +5,7 @@ import ProgressBar from "../UI/ProgressBar";
 import AnswersContent from "./types/AnswersContent.jsx";
 import prevIcon from "../../images/prevbutton.svg";
 import styles from "./Questionnaire.module.css";
- import Loader from "./types/Loader.jsx";
+import Loader from "./types/Loader.jsx";
 import QuestionnaireWrapper from "../../containers/QuestionnaireWrapper.jsx";
 import FormProgress from "../UI/FormProgress.jsx";
 import LogoPOS from "../UI/LogoPOS.jsx";
@@ -29,35 +29,33 @@ const Questionnaire = () => {
   const [showLoader, setShowLoader] = useState(false);
   const [isNextButtonClicked, setIsNextButtonClicked] = useState(false);
 
-
   const isFormSequence = currentQuestion.type === "form-type";
-
-  const progressBarWidth = Math.round(
-    (currentQuestion.step / 4) * 100
-  );
+  const isFinalStep = currentQuestionCode === "phone";
+  const isZipCodeStep = currentQuestionCode === "zip_code";
+  const isEmailStep = currentQuestionCode === "email";
+  const progressBarWidth = Math.round((currentQuestion.step / 4) * 100);
 
   useEffect(() => {
     let timerId;
     if (isNextButtonClicked) {
-        timerId = setTimeout(() => {
-            moveToNextQuestion();
-            setIsNextButtonClicked(false);
-        }, 500); 
+      timerId = setTimeout(() => {
+        moveToNextQuestion();
+        setIsNextButtonClicked(false);
+      }, 500);
     }
-    return () => clearTimeout(timerId); 
-}, [isNextButtonClicked, moveToNextQuestion]);
+    return () => clearTimeout(timerId);
+  }, [isNextButtonClicked, moveToNextQuestion]);
 
   useEffect(() => {
     checkAndEnableNextButton();
   }, [currentQuestion, responses]);
 
-  
   useEffect(() => {
-    let timeoutId = null; 
-    if (currentQuestion.type === 'loader') {
-      setShowLoader(true); 
+    let timeoutId = null;
+    if (currentQuestion.type === "loader") {
+      setShowLoader(true);
       timeoutId = setTimeout(() => {
-        setShowLoader(false); 
+        setShowLoader(false);
         const nextQuestionCode = currentQuestion.answers[0]?.next_question_code;
         if (nextQuestionCode) {
           setCurrentQuestionCode(nextQuestionCode);
@@ -73,8 +71,15 @@ const Questionnaire = () => {
 
   const handleNextButtonClick = () => {
     setIsNextButtonClicked(true); // Trigger the useEffect hook above
-};
+  };
+  const FinalStepTitle = ()=>{
+    return(
 
+      <div className={styles.titleWrapper}>
+        <h1 className={styles.finalStepTitle}>Final Step</h1>
+      </div>
+      )
+  }
   const QuestionnaireTitle = () => {
     return (
       <div className={styles.titleWrapper}>
@@ -85,56 +90,60 @@ const Questionnaire = () => {
       </div>
     );
   };
-  
-   if (showLoader) {
-     return (
-       <QuestionnaireLayout>
-         <QuestionnaireWrapper>
-           <Loader />
-         </QuestionnaireWrapper>
-       </QuestionnaireLayout>
-     );
-   }
-   return (
-     <QuestionnaireLayout>
-       {!questionnaireStarted &&<QuestionnaireTitle />}
-       {currentQuestionCode === "email" && <LogoPOS/>}
-        {isFormSequence &&<FormProgress/>}
-       <QuestionnaireWrapper>
-         {!isFormSequence && <ProgressBar width={progressBarWidth} />}
-         {currentQuestion.text && (
-           <div className={styles.questionDescriptionText}>
-             {currentQuestion.text}
-           </div>
-         )}
-         <div className={styles.contentWrapper}>
-           <AnswersContent />
-           {currentQuestionCode ==="phone" && ( 
-            <>
-           <ExtraInfo/>
-           <LegalMessage/>
-            </>
-           )}
-           <div className={styles.buttonsWrapper}>
-             {questionHistory.length > 0 && (
-               <button className={styles.prevBtn} onClick={moveToPrevQuestion}>
-                 <img src={prevIcon} alt="prev_btn_icon" />
-               </button>
-             )}
-             <button
-               className={`${styles.nextBtn} ${
-                 inputModified || nextBtnEnabled ? styles.enabled : ""
-               }`}
-               onClick={() => !isNextButtonFunctionallyDisabled && moveToNextQuestion()}
-               disabled={!inputModified && !nextBtnEnabled && !isNextButtonFunctionallyDisabled} 
-             >
-               Next
-             </button>
-           </div>
-         </div>
-       </QuestionnaireWrapper>
-     </QuestionnaireLayout>
-   );
+
+  if (showLoader) {
+    return (
+      <QuestionnaireLayout>
+        <QuestionnaireWrapper>
+          <Loader />
+        </QuestionnaireWrapper>
+      </QuestionnaireLayout>
+    );
+  }
+  return (
+    <QuestionnaireLayout>
+      {!questionnaireStarted && <QuestionnaireTitle />}
+      {isEmailStep && <LogoPOS />}
+      {isFormSequence && <FormProgress />}
+      <QuestionnaireWrapper>
+        {!isFormSequence && <ProgressBar width={progressBarWidth} />}
+        {isFinalStep && <FinalStepTitle/>}
+        {currentQuestion.text && (
+          <div className={styles.questionDescriptionText}>
+            {currentQuestion.text}
+          </div>
+        )}
+        <div className={styles.contentWrapper}>
+          <AnswersContent />
+          {(isFinalStep || isZipCodeStep) && <ExtraInfo />}
+
+          {isFinalStep && <LegalMessage />}
+          <div className={styles.buttonsWrapper}>
+            {questionHistory.length > 0 && (
+              <button className={styles.prevBtn} onClick={moveToPrevQuestion}>
+                <img src={prevIcon} alt="prev_btn_icon" />
+              </button>
+            )}
+            <button
+              className={`${styles.nextBtn} ${
+                inputModified || nextBtnEnabled ? styles.enabled : ""
+              }`}
+              onClick={() =>
+                !isNextButtonFunctionallyDisabled && moveToNextQuestion()
+              }
+              disabled={
+                !inputModified &&
+                !nextBtnEnabled &&
+                !isNextButtonFunctionallyDisabled
+              }
+            >
+              {isFinalStep ? 'Get Results' :'Next'}
+            </button>
+          </div>
+        </div>
+      </QuestionnaireWrapper>
+    </QuestionnaireLayout>
+  );
 };
 
 export default Questionnaire;
